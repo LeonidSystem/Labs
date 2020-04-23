0b344f925a74f5df1da950652cd97b93c4f841b0
LeonidSystem <44871773+LeonidSystem@users.noreply.github.com>
2020-04-23 09:48:57 +0400

    Create SimpleList.h

---
+++ SimpleList.h
@@ -0,0 +1,55 @@
+template <typename T>
+class SimpleList {
+    private:
+        struct node {
+            T val;
+            struct node *next = nullptr;
+            node(T value) : val(value) {};
+        } *first = nullptr, *last = nullptr;
+    public:
+        void push_back(T value) {
+            node *tmp = new struct node(value);
+            if (last!=nullptr) 
+                last->next = tmp;
+            last = tmp;
+            if (first==nullptr)
+                first = tmp;
+        };
+        
+        int find(T value) {
+            for (node *i = first, int j=0; i!=nullptr; j++, i = i->next)
+                if (i->val == value)
+                    return j;
+            return -1;        
+        };
+        
+        void Remove(T value) {
+            for (node *i = first; i!=nullptr; i = i->next)
+                if (i->next->val == val) {
+                    node *tmp = i->next;
+                    i->next = i->next->next;
+                    delete tmp;
+                    break;
+                };
+        };
+        
+        bool insert_after(T x, T y) {
+            for (node *i = first; i!=nullptr; i = i->next)
+                if (i->value==y) {
+                    node *tmp = new struct node(x);
+                    tmp->next = i->next;
+                    i->next = tmp;
+                    return true;
+                };
+            return false;
+        };
+        
+        ~SimpleList() {
+            node *tmp, *i = first;
+            while (i!=nullptr) {
+                tmp = i;
+                i = i->next;
+                delete tmp;
+            };
+        };
+};
